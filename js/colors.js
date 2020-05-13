8ff60b70e54c72ef8bd53974be89a741984eac24
teknostompa <47609054+teknostompa@users.noreply.github.com>
2020-05-13 15:15:30 +0200

    Add files via upload

---
+++ js/colors.js
@@ -0,0 +1,10 @@
+var colors = {
+    mainColor: "#E80C94",
+    textColor: "#000000",
+    darkColor: "#666666",
+    backgroundColor: "#FFFFFF"
+}
+document.documentElement.style.setProperty('--mainColor', colors.mainColor);
+document.documentElement.style.setProperty('--textColor', colors.textColor);
+document.documentElement.style.setProperty('--darkColor', colors.darkColor);
+document.documentElement.style.setProperty('--backgroundColor', colors.backgroundColor);
