6db2a5915297b06ce47ea349f934f0405af9b5ba
teknostompa <47609054+teknostompa@users.noreply.github.com>
2020-05-15 09:21:51 +0200

    Add files via upload

--- js/event.js
+++ js/event.js
@@ -1,11 +1,18 @@
 var events = [{
     eventName: "Tjejhack 2020",
-    eventTime: '04 Dec 2020 0:21'
+    eventTime: '04 Dec 2020 13:20'
+},{
+    eventName: "Tjejhack 2021",
+    eventTime: '04 Dec 2021 15:30'
+},{
+    eventName: "Tjejhack 2022",
+    eventTime: '04 Dec 2022 16:00'
 }]
 
 
 
 var nonExpiredEvents = [];
+var selectedEvent = 0;
 
 var d = new Date();
 var now = d.getTime();
@@ -19,8 +26,24 @@ for(var i = 0; i < events.length; i++){
         DateElement.innerHTML = events[i].eventTime;
         DescriptionElement.appendChild(DateElement);
         EventElement.appendChild(DescriptionElement);
+        EventElement.className = "Event";
+        document.getElementsByClassName("Events")[0].appendChild(EventElement);
     }
 }
+document.getElementsByClassName("Event")[0].className += " Selected";
+$(".Event").click(function() {
+    for(var i = 0; i < this.parentElement.childNodes.length; i++){
+        this.parentElement.childNodes[i].className = "Event";
+        if(this.parentElement.childNodes[i] == this){
+            selectedEvent = i - 1;
+        }
+    }
+    this.className += " Selected";
+    updateTime();
+    document.getElementsByClassName("TimerTitle")[0].innerHTML = nonExpiredEvents[selectedEvent].eventName;
+});
+
+document.getElementsByClassName("TimerTitle")[0].innerHTML = nonExpiredEvents[selectedEvent].eventName;
 
 var TimeElement = document.createElement("div");
 TimeElement.style.height = "100vh";
@@ -34,7 +57,7 @@ TextElement.style.justifyContent = "center";
 TextElement.style.alignItems = "center";
 Time.appendChild(TextElement);
 TimeElement.appendChild(Time);
-document.getElementsByClassName("Countdown")[0].appendChild(TimeElement)
+document.getElementsByClassName("Timer")[0].appendChild(TimeElement)
 
 
 updateTime();
@@ -42,10 +65,12 @@ updateTime();
 function updateTime(){
     var d = new Date();
     var now = d.getTime();
-    if(Date.parse(nonExpiredEvents[0].eventTime) - now > 0){
-        var timeleft = (Date.parse(nonExpiredEvents[0].eventTime) - now)/1000;
+    console.log(nonExpiredEvents[selectedEvent] + " " + selectedEvent);
+    if(Date.parse(nonExpiredEvents[selectedEvent].eventTime) - now > 0){
+        var timeleft = (Date.parse(nonExpiredEvents[selectedEvent].eventTime) - now)/1000;
 
-        var hours = Math.floor(timeleft/60/60);
+        var days = Math.floor(timeleft/60/60/24);
+        var hours = Math.floor(timeleft/60/60%24);
         var miniutes = Math.floor(timeleft/60%60);
         var seconds = Math.floor(timeleft%60);
 
@@ -55,9 +80,12 @@ function updateTime(){
         if(miniutes.toString().length < 2) {
             miniutes = "0" + miniutes;
         }
+        if(hours.toString().length < 2) {
+            hours = "0" + hours;
+        }
         
 
-        TextElement.innerHTML = hours + ":" + miniutes + ":" + seconds;
+        TextElement.innerHTML = days + ":" + hours + ":" + miniutes + ":" + seconds;
         setTimeout(function(){ updateTime(); }, 1000);
     }else{
         TextElement.innerHTML = "Eventet har börjat!";
